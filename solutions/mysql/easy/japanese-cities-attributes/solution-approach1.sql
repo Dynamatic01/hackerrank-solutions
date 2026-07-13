3f7c43cb138319d4791f4e409e8ce72b20f79e4d
Mohit <mehtamohit514@gmail.com>
2026-07-13 23:04:10 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-13, 11:04 p.m.
+-- ──────────────────────────────────────────────────
+
+select * from city where countrycode = "JPN";
