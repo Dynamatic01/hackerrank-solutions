b6f75552a3abe1ad69b5ebd68c571bb4b20f0059
Mohit <mehtamohit514@gmail.com>
2026-07-13 23:04:00 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-13, 11:03 p.m.
+-- ──────────────────────────────────────────────────
+
+select * from city where id= 1661;
