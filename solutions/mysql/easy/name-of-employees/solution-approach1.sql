9ef22bc82e4050ee63452a3b817843fa1efd9f07
Mohit <mehtamohit514@gmail.com>
2026-07-14 12:25:06 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-14, 12:25 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+SELECT name
+FROM Employee
+ORDER BY name; 
